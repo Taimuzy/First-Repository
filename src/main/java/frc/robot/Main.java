591eecde084e356c8510964e6647e21766b5c26d
ComputadorDriverStation <sesi.techmakerrobotics@gmail.com>
2024-05-31 14:41:00 -0300

    Second Commit

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -21,24 +21,5 @@ public final class Main {
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
-
-
-    /*hjvwdiuqBCJENE  WEVEW
-     * FIRSTWE
-     * 
-     * Wefwef
-     * Wefweffwewe
-     * 
-     * we
-     * 
-     * ewf
-     * we
-     * fwe
-     * filewfe
-     * we
-     * 
-     * ewfew
-     * ew
-     */
   }
 }
